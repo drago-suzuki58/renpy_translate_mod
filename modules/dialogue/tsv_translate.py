df2e089100f8d4e7cd692712f46a9e0b65cc7602
drago-suzuki58 <dorasuzu58@gmail.com>
2024-06-09 15:21:09 +0900

    feat: セリフの翻訳処理を追加
    それに加えてインポートファイルとアウトプットファイルの無視設定も追加

--- modules/dialogue/tsv_translate.py
+++ modules/dialogue/tsv_translate.py
@@ -1,2 +1,71 @@
-def main():
-    pass
+from typing import List
+from googletrans import Translator
+import re
+import requests
+
+import modules.run_logs as logs
+import modules.base16 as base16
+
+def main(input: str, output: str, fromlang: str, tolang: str, target: List[str]):
+    logs.logs("DEBUG", "Translate_dialogue", f"{input}, {output}, {fromlang}, {tolang}, {target}")
+
+    lines_to_write = []
+    progress = 0
+
+    if fromlang == tolang:
+        logs.logs("ERROR", "Translate_dialogue", "The source and target languages are the same.")
+        return
+
+    g_translator = Translator()
+    logs.logs("INFO", "Translate_dialogue", f"Translor initialized")
+
+    with open(input, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+        total_lines = len(lines)
+        logs.logs("INFO", "Translate_dialogue", f"Read {len(lines)} lines from {input}")
+
+        for line in lines:
+            if line == "filename\tlinenumber\tidentifier\tcontents\n":
+                continue # ヘッダー行をスキップ
+
+            filename, linenumber, identifier, contents = line.strip().split('\t')
+
+            if target and filename not in target:
+                continue
+
+            match_str = re.match(r'^([^"]*)"((?:[^"\\]|\\.)*)"', contents) # エスケープされていないダブルクォーテーションのみを対象にする
+            if match_str:
+                person = match_str.group(1)
+                dialogue = match_str.group(2)
+                original_dialogue = dialogue
+                logs.logs("DEBUG", "Translate_dialogue", f"Extracted: {person},{dialogue}")
+
+                dialogue = base16.encode_matches(r'\{.*?\}', dialogue)
+                dialogue = base16.encode_matches(r'\[.*?\]', dialogue)
+                dialogue = base16.encode_matches(r'\\.', dialogue) # \{encoded_text} の形式だと変換されてしまうと思うので|encoded_text|のように変換
+
+                for _ in range(5): # 翻訳のタイムアウト時に最大5回再試行
+                    try:
+                        translated_text = g_translator.translate(dialogue, src=fromlang, dest=tolang).text
+                        logs.logs("INFO", "Translate_dialogue", f"Translated: {original_dialogue} -> {translated_text}")
+                        break
+                    except requests.exceptions.Timeout:
+                        continue
+                else:
+                    logs.logs("ERROR", "Translate_dialogue", f"Google Translate API timed out 5 times. Skipping this line.\nLine: {progress}")
+                    continue
+
+                translated_text = base16.decode_matches(translated_text)
+
+                contents = f'{person}"{translated_text}"'
+                lines_to_write.append(f"{filename}\t{linenumber}\t{identifier}\t{contents}\n")
+                logs.logs("DEBUG", "Translate_dialogue", f"Append: {filename}, {linenumber}, {identifier}, {contents}")
+
+                logs.logs("INFO", "Translate_dialogue", f"Line {progress}/{total_lines}, {contents}")
+                progress += 1
+
+    with open(output, "w", encoding="utf-8") as f:
+        f.write("filename\tlinenumber\tidentifier\tcontents\n")
+        for line in lines_to_write:
+            f.write(line)
+        logs.logs("INFO", "Translate_dialogue", f"Wrote {len(lines_to_write)} lines to {output}")
