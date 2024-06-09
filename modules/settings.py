e8f21df5e0fbae77fea48d5e0d78787cf19a8136
drago-suzuki58 <dorasuzu58@gmail.com>
2024-06-09 13:57:12 +0900

    feat: 設定ファイルを詳細に変更
    コマンドのデフォルト引数をsettings.pyを参照するように変更し、何度も同じオプションを打ち込まなくてもよくした。

--- modules/settings.py
+++ modules/settings.py
@@ -1,3 +1,34 @@
+# Log
 LOG_ENABLED = False
 LOG_FILE = 'log.txt'
 LOG_LEVEL = 'INFO'
+
+# TSV Translate
+TSV_TRANSLATE_INPUT = 'untranslated.tsv'
+TSV_TRANSLATE_OUTPUT = 'translated.tsv'
+
+# TSV2RPY
+TSV2RPY_INPUT = 'translated.tsv'
+TSV2RPY_OUTPUT = 'converted.rpy'
+
+# Extract Menu
+EXTRACT_MENU_INPUT = []
+EXTRACT_MENU_OUTPUT = 'menu_untranslated.tsv'
+
+# TSV Translate Menu version
+MENU_TSV_TRANSLATE_INPUT = 'menu_untranslated.tsv'
+MENU_TSV_TRANSLATE_OUTPUT = 'menu_translated.tsv'
+
+# TSV2RPY Menu version
+MENU_TSV2RPY_INPUT = 'menu_translated.tsv'
+MENU_TSV2RPY_OUTPUT = 'menu_converted.rpy'
+
+# Translation common settings
+FROM_LANG = 'en'
+TO_LANG = 'ja'
+
+# TSV2RPY common settings
+TARGET = []
+TL_LANG = 'japanese'
+
+COMMENT = False
