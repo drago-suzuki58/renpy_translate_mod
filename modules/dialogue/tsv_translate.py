ac9981ec39f06b8c18afd7163aadaec02c1baa47
drago-suzuki58 <dorasuzu58@gmail.com>
2024-06-13 11:44:52 +0900

    feat: 翻訳後ファイルを、途中で処理が終わっても途中経過を出力できるように改善

--- modules/dialogue/tsv_translate.py
+++ modules/dialogue/tsv_translate.py
@@ -9,7 +9,6 @@ import modules.base16 as base16
 def main(input: str, output: str, fromlang: str, tolang: str, target: List[str]):
     logs.logs("DEBUG", "Translate_dialogue", f"{input}, {output}, {fromlang}, {tolang}, {target}")
 
-    lines_to_write = []
     progress = 1
 
     if fromlang == tolang:
@@ -20,61 +19,57 @@ def main(input: str, output: str, fromlang: str, tolang: str, target: List[str])
     logs.logs("INFO", "Translate_dialogue", f"Translor initialized")
 
     with open(input, "r", encoding="utf-8") as f:
-        lines = f.readlines()
-        total_lines = len(lines) - 1
-        logs.logs("INFO", "Translate_dialogue", f"Read {len(lines)} lines from {input}")
+        with open(output, "w", encoding="utf-8") as output_f:
+            lines = f.readlines()
+            total_lines = len(lines) - 1
+            output_f.write("filename\tlinenumber\tidentifier\tcontents\n") # ヘッダー行を追加
+            logs.logs("INFO", "Translate_dialogue", f"Read {len(lines)} lines from {input}")
 
-        for line in lines:
-            if line == "filename\tlinenumber\tidentifier\tcontents\n":
-                continue # ヘッダー行をスキップ
+            for line in lines:
+                if line == "filename\tlinenumber\tidentifier\tcontents\n":
+                    continue # ヘッダー行をスキップ
 
-            filename, linenumber, identifier, contents = line.strip().split('\t')
+                filename, linenumber, identifier, contents = line.strip().split('\t')
 
-            if target and filename not in target:
-                continue
-
-            match_str = re.match(r'^([^"]*)"((?:[^"\\]|\\.)*)"', contents) # エスケープされていないダブルクォーテーションのみを対象にする(つまりセリフと人物を分ける)
-
-            if match_str == None:
-                lines_to_write.append(f"{filename}\t{linenumber}\t{identifier}\t{contents}\n")
-
-                logs.logs("DEBUG", "Translate_dialogue", f"Append:\t{filename}, {linenumber}, {identifier}, {contents}")
-                logs.logs("INFO", "Translate_dialogue", f"Line {progress}/{total_lines}:\t{contents}")
-
-                progress += 1
-
-            else:
-                person = match_str.group(1)
-                dialogue = match_str.group(2)
-                original_dialogue = dialogue
-                logs.logs("DEBUG", "Translate_dialogue", f"Extracted:\t{person},{dialogue}")
-
-                dialogue = base16.encode_matches(r'\{.*?\}', dialogue)
-                dialogue = base16.encode_matches(r'\[.*?\]', dialogue)
-                dialogue = base16.encode_matches(r'\\.', dialogue)
-
-                for _ in range(5): # 翻訳のタイムアウト時に最大5回再試行
-                    try:
-                        translated_text = g_translator.translate(dialogue, src=fromlang, dest=tolang).text
-                        logs.logs("INFO", "Translate_dialogue", f"Translated:\t{original_dialogue} -> {translated_text}")
-                        break
-                    except requests.exceptions.Timeout:
-                        continue
-                else:
-                    logs.logs("ERROR", "Translate_dialogue", f"Google Translate API timed out 5 times. Skipping this line.\nLine: {progress}")
+                if target and filename not in target:
                     continue
 
-                translated_text = base16.decode_matches(translated_text)
+                match_str = re.match(r'^([^"]*)"((?:[^"\\]|\\.)*)"', contents) # エスケープされていないダブルクォーテーションのみを対象にする(つまりセリフと人物を分ける)
 
-                contents = f'{person}"{translated_text}"'
-                lines_to_write.append(f"{filename}\t{linenumber}\t{identifier}\t{contents}\n")
-                logs.logs("DEBUG", "Translate_dialogue", f"Append:\t{filename}, {linenumber}, {identifier}, {contents}")
+                if match_str == None:
+                    output_f.write(f"{filename}\t{linenumber}\t{identifier}\t{contents}\n")
 
-                logs.logs("INFO", "Translate_dialogue", f"Line {progress}/{total_lines}:\t{contents}")
-                progress += 1
+                    logs.logs("DEBUG", "Translate_dialogue", f"Append:\t{filename}, {linenumber}, {identifier}, {contents}")
+                    logs.logs("INFO", "Translate_dialogue", f"Line {progress}/{total_lines}:\t{contents}")
 
-    with open(output, "w", encoding="utf-8") as f:
-        f.write("filename\tlinenumber\tidentifier\tcontents\n") # ヘッダー行を追加
-        for line in lines_to_write:
-            f.write(line)
-        logs.logs("INFO", "Translate_dialogue", f"Wrote {len(lines_to_write)} lines to {output}")
+                    progress += 1
+
+                else:
+                    person = match_str.group(1)
+                    dialogue = match_str.group(2)
+                    original_dialogue = dialogue
+                    logs.logs("DEBUG", "Translate_dialogue", f"Extracted:\t{person},{dialogue}")
+
+                    dialogue = base16.encode_matches(r'\{.*?\}', dialogue)
+                    dialogue = base16.encode_matches(r'\[.*?\]', dialogue)
+                    dialogue = base16.encode_matches(r'\\.', dialogue)
+
+                    for _ in range(5): # 翻訳のタイムアウト時に最大5回再試行
+                        try:
+                            translated_text = g_translator.translate(dialogue, src=fromlang, dest=tolang).text
+                            logs.logs("INFO", "Translate_dialogue", f"Translated:\t{original_dialogue} -> {translated_text}")
+                            break
+                        except requests.exceptions.Timeout:
+                            continue
+                    else:
+                        logs.logs("ERROR", "Translate_dialogue", f"Google Translate API timed out 5 times. Skipping this line.\nLine: {progress}")
+                        continue
+
+                    translated_text = base16.decode_matches(translated_text)
+
+                    contents = f'{person}"{translated_text}"'
+                    output_f.write(f"{filename}\t{linenumber}\t{identifier}\t{contents}\n")
+                    logs.logs("DEBUG", "Translate_dialogue", f"Append:\t{filename}, {linenumber}, {identifier}, {contents}")
+
+                    logs.logs("INFO", "Translate_dialogue", f"Line {progress}/{total_lines}:\t{contents}")
+                    progress += 1
--- modules/menu/tsv_translate.py
+++ modules/menu/tsv_translate.py
@@ -9,7 +9,6 @@ import modules.base16 as base16
 def main(input: str, output: str, fromlang: str, tolang: str, target: List[str]):
     logs.logs("DEBUG", "Translate_menu", f"{input}, {output}, {fromlang}, {tolang}, {target}")
 
-    lines_to_write = []
     progress = 1
 
     if fromlang == tolang:
@@ -20,48 +19,44 @@ def main(input: str, output: str, fromlang: str, tolang: str, target: List[str])
     logs.logs("INFO", "Translate_dialogue", f"Translor initialized")
 
     with open(input, "r", encoding="utf-8") as f:
-        lines = f.readlines()
-        total_lines = len(lines) - 1
-        logs.logs("INFO", "Translate_dialogue", f"Read {len(lines)} lines from {input}")
+        with open(output, "w", encoding="utf-8") as output_f:
+            lines = f.readlines()
+            total_lines = len(lines) - 1
+            f.write("filename\tlinenumber\tchoice\ttranslated\n") # ヘッダー行を追加
+            logs.logs("INFO", "Translate_dialogue", f"Read {len(lines)} lines from {input}")
 
-        for line in lines:
-            if line == "filename\tlinenumber\tchoice\ttranslated\n":
-                continue # ヘッダー行をスキップ
+            for line in lines:
+                if line == "filename\tlinenumber\tchoice\ttranslated\n":
+                    continue # ヘッダー行をスキップ
 
-            filename, linenumber, choice , translated_text= line.strip().split('\t')
+                filename, linenumber, choice , translated_text= line.strip().split('\t')
 
-            if target and filename not in target:
-                continue
-
-            if choice:
-                original_choice = choice
-                logs.logs("DEBUG", "Translate_dialogue", f"Extracted:\t{choice}")
-
-                choice = base16.encode_matches(r'\{.*?\}', choice)
-                choice = base16.encode_matches(r'\[.*?\]', choice)
-                choice = base16.encode_matches(r'\\.', choice) # \{encoded_text} の形式だと変換されてしまうと思うので|encoded_text|のように変換
-
-                for _ in range(5): # 翻訳のタイムアウト時に最大5回再試行
-                    try:
-                        translated_text = g_translator.translate(choice, src=fromlang, dest=tolang).text
-                        logs.logs("INFO", "Translate_dialogue", f"Translated:\t{original_choice} -> {translated_text}")
-                        break
-                    except requests.exceptions.Timeout:
-                        continue
-                else:
-                    logs.logs("ERROR", "Translate_dialogue", f"Google Translate API timed out 5 times. Skipping this line.\nLine: {progress}")
+                if target and filename not in target:
                     continue
 
-                translated_text = base16.decode_matches(translated_text)
+                if choice:
+                    original_choice = choice
+                    logs.logs("DEBUG", "Translate_dialogue", f"Extracted:\t{choice}")
 
-                lines_to_write.append(f"{filename}\t{linenumber}\t{original_choice}\t{translated_text}\n")
-                logs.logs("DEBUG", "Translate_dialogue", f"Append:\t{filename}, {linenumber}, {original_choice}, {translated_text}")
+                    choice = base16.encode_matches(r'\{.*?\}', choice)
+                    choice = base16.encode_matches(r'\[.*?\]', choice)
+                    choice = base16.encode_matches(r'\\.', choice) # \{encoded_text} の形式だと変換されてしまうと思うので|encoded_text|のように変換
 
-                logs.logs("INFO", "Translate_dialogue", f"Line {progress}/{total_lines}:\t{original_choice},\t{translated_text}")
-                progress += 1
+                    for _ in range(5): # 翻訳のタイムアウト時に最大5回再試行
+                        try:
+                            translated_text = g_translator.translate(choice, src=fromlang, dest=tolang).text
+                            logs.logs("INFO", "Translate_dialogue", f"Translated:\t{original_choice} -> {translated_text}")
+                            break
+                        except requests.exceptions.Timeout:
+                            continue
+                    else:
+                        logs.logs("ERROR", "Translate_dialogue", f"Google Translate API timed out 5 times. Skipping this line.\nLine: {progress}")
+                        continue
 
-    with open(output, "w", encoding="utf-8") as f:
-        f.write("filename\tlinenumber\tchoice\ttranslated\n") # ヘッダー行を追加
-        for line in lines_to_write:
-            f.write(line)
-        logs.logs("INFO", "Translate_dialogue", f"Wrote {len(lines_to_write)} lines to {output}")
+                    translated_text = base16.decode_matches(translated_text)
+
+                    output_f.write(f"{filename}\t{linenumber}\t{original_choice}\t{translated_text}\n")
+                    logs.logs("DEBUG", "Translate_dialogue", f"Append:\t{filename}, {linenumber}, {original_choice}, {translated_text}")
+
+                    logs.logs("INFO", "Translate_dialogue", f"Line {progress}/{total_lines}:\t{original_choice},\t{translated_text}")
+                    progress += 1
