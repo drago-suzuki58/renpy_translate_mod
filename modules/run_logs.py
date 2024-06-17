d6da4ee99d36afc749507896cf200c6a734b64d8
drago-suzuki58 <dorasuzu58@gmail.com>
2024-06-17 22:27:08 +0900

    feat: ログの色分けを追加

--- modules/run_logs.py
+++ modules/run_logs.py
@@ -1,6 +1,9 @@
 import datetime
+from colorama import Fore, Style, init
 
 import modules.settings as settings
+
+init(autoreset=True)
 
 def initialize_logfile():
     with open(settings.LOG_FILE, 'w', encoding='utf-8') as f:
@@ -14,10 +17,27 @@ def logs(level, description, text=""):
     if settings.LOG_ENABLED:
         # ログレベルで、そのレベル以上のログのみ出力する
         if level in valid_levels and level_mapping[level] >= level_mapping[settings.LOG_LEVEL]:
-            print(f"{level}:\t{description}:\t{text}")
+            print_logs(level, f"{description}:\t{text}")
             with open(settings.LOG_FILE, 'a', encoding='utf-8') as f:
                 current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
                 f.write(f"{current_time}:{level}:\t{description}:\t{text}\n")
 
         elif level not in valid_levels:
             logs("ERROR", "Logs", f"Invalid log level '{level}'")
+
+def print_logs(level, texts):
+
+    if level == "DEBUG":
+        print(Fore.BLUE + Style.BRIGHT + f"DEBUG:\t{texts}")
+
+    elif level == "INFO":
+        print(f"INFO:\t{texts}")
+
+    elif level == "WARNING":
+        print(Fore.YELLOW + Style.BRIGHT + f"WARNING:\t{texts}")
+
+    elif level == "ERROR":
+        print(Fore.RED + Style.BRIGHT + f"ERROR:\t{texts}")
+
+    elif level == "CRITICAL":
+        print(Fore.MAGENTA + Style.BRIGHT + f"CRITICAL:\t{texts}")
