cf6904a9679c70c91543f0149dc8eeac9a4b5b0c
drago-suzuki58 <dorasuzu58@gmail.com>
2024-06-19 03:26:43 +0900

    upgrade: バージョン番号を変更

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from cx_Freeze import setup, Executable
 # Python script to executable file
 setup(
     name = "RenPy Translate Tools",
-    version = "0.1",
+    version = "1.1.0",
     description = "This script allows for the handling of RenPy dialogues and choices as TSV data, enabling automatic translation and the ability to play the game in a different language.",
     executables = [Executable("main.py")]
 )
